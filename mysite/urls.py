1853c9eb039c3f3af6530147067bc2445a7856ba
Ohad Zmiry <oazmiry@gmail.com>
2020-03-22 12:08:10 +0200

    Part 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # TODO: Check what happens when sending a POST to a route to which  two routes are matching,  and only the second
-    #  one handles the POST verb.
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
